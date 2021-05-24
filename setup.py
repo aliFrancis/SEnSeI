76d778afd733515df39c10b50bc06b598fbc47b1
aliFrancis <alistair.m.francis@gmail.com>
2021-05-24 13:02:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='sensei',
    description='Spectral Encoding for Sensor Independence',
-   author='Alistair Francis',
+   author='Alistair Francis and John Mrziglod',
    author_email='a.francis.16@ucl.ac.uk',
    packages=['sensei',
              'sensei/data'
